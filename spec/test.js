adafd07821881fa486a51f9023235a19c889fe42
ryanshim24 <ryanshim24@yahoo.com>
2014-10-10 03:19:29 -0700

    Not sure if I did it correctly used multiple ways

--- spec/test.js
+++ spec/test.js
@@ -4,53 +4,82 @@ var expect = require("chai").expect;
 // all of these tests are pending...make sure to add , function(){ expect ....}
 
 describe('first', function() {
-  it('should be able to pull out the first element of an array');
-    // expect(myFunctions.first([1,2,3])).to.equal(1);
-
-
-  it('should be able to accept a user-defined index first([1,2,3],2) should return [1,2]');
-
+  var myArr;
+  beforeEach(function() {
+    myArr = [1,2,3];
   });
+  it('should be able to pull out the first element of an array', function(){
+    // expect(myFunctions.first([1,2,3])).to.equal(1);
+    expect(myFunctions.first(myArr)).to.equal(1);
+  });
+  it('should be able to accept a user-defined index first([1,2,3],2) should return [1,2]', function() {
+    expect(myFunctions.first(myArr, 2)).to.eql([1,2]);
+  });
+});
 
 
 describe('last', function() {
-  it('should pull the last element from an array');
+  var myArr;
+  beforeEach(function () {
+    myArr = [1,2,3,4,5];
+  });
+  it('should pull the last element from an array', function() {
+    expect(myFunctions.last(myArr)).to.equal(5);
+  });
 
-  it('should accept an index argument');
+  it('should accept an index argument', function() {
+    expect(myFunctions.last(myArr,3)).to.eql([5,4,3]);
+  });
+  it('should return nothing if zero is passed in as the index', function() {
+    expect(myFunctions.last(myArr,0)).to.eql([]);
+  });
 
-  it('should return nothing if zero is passed in as the index');
-
-  it('should return all the array\'s elements if the index argument is larger than the length of the array');
-
+  it('should return all the array\'s elements if the index argument is larger than the length of the array',function() {
+    expect(myFunctions.last(myArr,6)).to.eql([5,4,3,2,1]);
+  });
 });
 
 describe('uniq', function() {
-  it('should return all unique values contained in an unsorted array');
-
+  var myArr;
+  beforeEach(function() {
+    myArr = [1,4,22,1,17,23,22];
+  });
+  it('should return all unique values contained in an unsorted array',function() {
+    expect(myFunctions.uniq(myArr)).to.eql([1,4,17,22,23]);
+  });
 });
 
 
 describe('pluck', function() {
-  it('should return values contained at a user-defined property');
-    // var people = [
-    //   {name : 'moe', age : 30},
-    //   {name : 'curly', age : 50}
-    // ];
+  it('should return values contained at a user-defined property', function () {
+    var people = [
+      {name : 'moe', age : 30},
+       {name : 'curly', age : 50}
+     ];
 
-    // expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+    expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+  });
 });
 
 describe('contains', function() {
-  it('should return false if a collection does not contain a user-specified value');
+  var myArr;
+  beforeEach(function(){
+    myArr = [1,2,3,6,5];
+  });
+  it('should return false if a collection does not contain a user-specified value', function(){
+    expect(myFunctions.contains(myArr,7)).to.equal(false);
+  });
 
-  it('should return true if a collection contains a user-specified value');
-    // expect(myFunctions.contains([  4,   5,   6], 5)).to.equal(true);
+  it('should return true if a collection contains a user-specified value',function(){
+    expect(myFunctions.contains([  4,   5,   6], 5)).to.equal(true);
+  });
 
 });
 
-describe('flatten', function() {
-  it('can flatten nested arrays');
-    // var nestedArray = [1, [2], [3, [[[4]]]]];
+// describe('flatten', function() {
+//   it('can flatten nested arrays', function() {
+//     var nestedArray = [1, [2], [3, [[[4]]]]];
 
-    // expect(myFunctions.flatten(nestedArray)).to.eql([1,2,3,4]);
-});
+//     expect(myFunctions.flatten(nestedArray)).to.eql([1,2,3,4]);
+//   });
+// });
--- src/underscore.js
+++ src/underscore.js
@@ -3,31 +3,98 @@ var myFunctions = {
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   first: function(array, n) {
-
+    var newArray = [];
+    if (n === undefined) {
+      return array[0];
+    }else {
+        var filtered = array.filter(function(num) {
+          return array.indexOf(num) < n;
+        });
+        return filtered;
+      }
+    // } else {
+    //   for (var i =0; i<n; i++) {
+    //     newArray.push(array[i]);
+    //   }
+    //   return newArray;
   },
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   last: function(array, n) {
-
-  },
+    var newArray = [];
+    var length = array.length;
+    if (n === undefined) {
+      return array.pop();
+  }else if (n > length) {
+      var filtered = array.filter(function (num) {
+        return array.indexOf(num) < n;
+      });
+      return filtered.reverse();
+    }else if (n === 0) {
+      var zeroed = array.filter(function (num) {
+        return array.indexOf(num)<0;
+      });
+      return zeroed.reverse();
+    }else {
+    var sorted = array.filter(function (num) {
+      return array.indexOf(num) >= n-1;  
+    });
+    return sorted.reverse();
+  }
+    // } else if (n > length) {
+    //   for (var i=length-1; i>-1; i--) {
+    //     newArray.push(array[i]);
+    //   }
+    //   return newArray;
+    // } else {
+    //   length = array.length-1;
+    //   for (var j=0; n>j; j++) {
+    //     newArray.push(array[length-j]);
+    //   }
+    //   return newArray;
+    // }          
+},
 
   // Produce a duplicate-free version of the array.
   uniq: function(array) {
-
+     var sorted = array.sort(function (a, b) {
+        return a - b;
+    });
+     var filtered = [];
+     for(i=0;i<sorted.length;i++) {
+      if(sorted[i-1] !== sorted[i]){    
+        filtered.push(sorted[i]);
+      }
+    }
+    return filtered;
   },
 
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people objects
   // (which have a name and an age) and return an array of just their ages
   pluck: function(array, key) {
-
+    if (key === "name"){
+       var sorted = array.map(function(obj){
+        return obj.name;
+      });
+      return sorted;
+    }else {
+      var numbered =array.map(function(obj){
+        return obj.age;
+      });
+      return numbered;
+    }
   },
 
  // Determine if the array contains a given value (using `===`).
  // TIP: There are multiple solutions to this problem, ranging from one line to using reduce();
   contains: function(array, target) {
-
+    if (array.indexOf(target) > -1) {
+      return true;
+    }else {
+      return false;
+    }
   },
 
   // Turns a multidimensional array into a single array
